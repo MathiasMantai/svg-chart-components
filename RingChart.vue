a27b3b5d7793f1705db3f4c6715fd6876aab0511
Mantai59 <mmantaibusiness@gmail.com>
2022-12-19 20:53:55 +0100

    calculating angles based on percentage of 360 degrees;

--- RingChart.vue
+++ RingChart.vue
@@ -1,12 +1,20 @@
 <script>
-    import RingChart from './RingChart.jsx';
-
     export default {
+        data() {
+            return {
+                totalData: 0,
+                svgRingParts: []
+            }
+        },
         props: {
             radius: {
                 type: Number,
                 default: 100
-            }
+            },
+            data: {
+                type: Array,
+                default: [10,10,10,10,10]
+            },
         },
         methods: {
             arcEndPoint(angle, radius, totalSize) {
@@ -14,13 +22,37 @@
                     Math.cos(angle * Math.PI / 180) * radius + totalSize / 2,
                     Math.sin(angle * Math.PI / 180) * -radius + totalSize / 2
                 ]
+            },
+            calcTotalData(data) {
+                let tmp = 0;
+                data.map(dataElement => tmp += dataElement);
+                return tmp;
+            },
+            dataToAngle(dataElement, totalData) {
+                let dataElementPercentage = (dataElement * 100) / totalData;
+                let angle = (dataElementPercentage * 360) / 100;
+                console.log(dataElement, totalData);
+                return angle;
+            },
+            outputChart() {
+                return h('div', {id: 'test'}, []);
             }
+        },
+        mounted() {
+            //calculate sum of data
+            this.totalData = this.calcTotalData(this.data);
+            
+            //calculate angles based on percentage of 360 degrees
+            this.data.map( (dataElement) => {
+                this.svgRingParts.push(this.dataToAngle(dataElement, this.totalData));
+            });
+
+            //create ring parts command
+
         }
     }
 </script>
 
 <template>
-    <div>
-        {{arcEndPoint(45, radius, 300)}}
-    </div>
+    {{ svgRingParts }}
 </template>
