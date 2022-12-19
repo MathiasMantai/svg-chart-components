db8035f97d0bfdc934facfc9ab5fb800a3233399
Mantai59 <mmantaibusiness@gmail.com>
2022-12-19 19:02:29 +0100

    Ringchart - calculating endpoint of arc

---
+++ RingChart.vue
@@ -0,0 +1,26 @@
+<script>
+    import RingChart from './RingChart.jsx';
+
+    export default {
+        props: {
+            radius: {
+                type: Number,
+                default: 100
+            }
+        },
+        methods: {
+            arcEndPoint(angle, radius, totalSize) {
+                return  [
+                    Math.cos(angle * Math.PI / 180) * radius + totalSize / 2,
+                    Math.sin(angle * Math.PI / 180) * -radius + totalSize / 2
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        {{arcEndPoint(45, radius, 300)}}
+    </div>
+</template>
