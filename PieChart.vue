54ddb5bf5fede56622f70a603963042a124383b3
MathiasMantai <mmantaibusiness@gmail.com>
2022-12-21 09:07:40 +0100

    piechart: offset and large arc flag fix

--- PieChart.vue
+++ PieChart.vue
@@ -37,7 +37,7 @@
             dataToAngle(dataElement, totalData) {
                 let dataElementPercentage = (dataElement * 100) / totalData;
                 let angle = (dataElementPercentage * 360) / 100;
-                console.log(dataElement, totalData);
+                // console.log(dataElement, totalData);
                 return angle;
             },
             outputChart() {
@@ -54,25 +54,31 @@
             let angleSum = 0;
             let cnt = 0;
             this.data.map( (dataElement) => {
-                angleSum += this.dataToAngle(dataElement, this.totalData);
-                let currentEndPoint = this.arcEndPoint(angleSum, this.radius, this.radius * 2);
-                this.svgPieParts.push({path: "M " + startX + " " + startY + " " + "A " + this.radius + " " + this.radius + " 0 0 0 " + currentEndPoint[0] + " " + currentEndPoint[1] + " L " + this.radius + " " + this.radius, color: this.color[cnt]});
+                let currentAngle = this.dataToAngle(dataElement, this.totalData);
+                let largeArcFlag = (currentAngle > 180 ? 1 : 0);
 
-                startX = currentEndPoint[0];
-                startY = currentEndPoint[1];
+                //offset for 
+                let offset = 360 - angleSum;
+
+                //sum of angles for next offset
+                angleSum += currentAngle;
+
+                //endpoint for outer arc
+                let currentEndPoint = this.arcEndPoint(currentAngle, this.radius, this.radius * 2);
+
+                //path commands
+                this.svgPieParts.push({path: "M " + startX + " " + startY + " " + "A " + this.radius + " " + this.radius + " 0 " + largeArcFlag + " 0 " + currentEndPoint[0] + " " + currentEndPoint[1] + " L " + this.radius + " " + this.radius, color: this.color[cnt], offset: offset});
                 cnt++;
             });
 
+            //viewbox
             this.viewbox = "0 0 " + this.radius*2 + " " + this.radius*2;
-
-
-
         }
     }
 </script>
 
 <template>
     <svg :viewbox="viewbox" :width="(radius * 2)" :height="(radius * 2)">
-        <path v-for="path in svgPieParts" :d="path.path" :fill="path.color" />
+        <path v-for="path in svgPieParts" :d="path.path" :fill="path.color" style="transform-origin: center;" :transform="'rotate('+path.offset+')'" />
     </svg>
 </template>
